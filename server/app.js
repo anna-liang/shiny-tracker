508e66973839b57095a53fc98a4406401bf94f59
anna-liang <annaliang56@gmail.com>
2021-09-17 00:09:01 -0400

    Set new cookies and adding new hunt

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ app.use(bodyParser.json());
 
 const cookie = require('cookie');
 
-const session = require('express-session');
+// const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
@@ -16,16 +19,16 @@ app.use(cors({
   optionsSuccessStatus: 200,
 }));
 
-app.use(session({
-    secret: 'pokemon',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        secure: true,
-        sameSite: true
-    }
-}));
+// app.use(session({
+//     secret: 'pokemon',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         httpOnly: true,
+//         secure: true,
+//         sameSite: true
+//     }
+// }));
 
 var Hunt = (function() {
     let time = Date.now();
@@ -33,24 +36,24 @@ var Hunt = (function() {
     return function item(target, count, gen, method, phase, charm, active) {
         this.target = target;
         this.count = count;
-        this.gen = gen;
-        this.method = method;
-        this.phase = phase;
-        this.charm = charm;
-        this.active = active;
+        // this.gen = gen;
+        // this.method = method;
+        // this.phase = phase;
+        // this.charm = charm;
+        // this.active = active;
         this.date = date;
     };
 }());
 
 app.use(function (req, res, next){
     console.log("HTTP request", req.method, req.url, req.body);
-    var isSessionSet = req.session.isSessionSet;
-    res.setHeader('Set-Cookie', cookie.serialize('init', isSessionSet, {
-        path : '/', 
-        maxAge: 60 * 60 * 24 * 7, // 1 week in number of seconds
-        secure: true,
-        sameSite: true
-    }));
+    // var isSessionSet = req.session.isSessionSet;
+    // res.setHeader('Set-Cookie', cookie.serialize('init', isSessionSet, {
+    //     path : '/', 
+    //     maxAge: 60 * 60 * 24 * 7, // 1 week in number of seconds
+    //     secure: true,
+    //     sameSite: true
+    // }));
     next();
 });
 
@@ -58,18 +61,28 @@ app.use(function (req, res, next){
 app.post('/api/hunt/', function (req, res, next) {
     // let hunt = new Hunt(req.target, req.count, req.gen, req.method, req.phase, req.charm, req.active);
     // !!! needs to be a list of hunts (pokemon name acts as id)
-    console.log("NEW HUNT");
-    req.session.isSessionSet = true;
-    res.setHeader('Set-Cookie', cookie.serialize('target', req.body.target, {
-        path: '/',
-        maxAge: 60 * 60 * 24 * 7
-    }));
-    res.setHeader('Set-Cookie', cookie.serialize('count', req.body.count, {
-        path: '/',
-        maxAge: 60 * 60 * 24 * 7
-    }));
-    // res.setHeader('target', req.target);
-    // res.setHeader('count', req.count);
+    var hunt = new Hunt(
+        req.body.target,
+        req.body.count
+    );
+    console.log("NEW HUNT", hunt);
+    console.log(req.cookies);
+    if (req.cookies.hunts == undefined) {
+        console.log("no hunts");
+        res.setHeader('Set-Cookie', cookie.serialize('hunts', JSON.stringify([]), {
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7
+        }));
+    }
+    else {
+        var newHunts = JSON.parse(req.cookies.hunts);
+        newHunts.push(hunt);
+        console.log(newHunts);
+        res.setHeader('Set-Cookie', cookie.serialize('hunts', newHunts, {
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7
+        }));
+    }
     // res.setHeader('Access-Control-Allow-Credentials', true);
     console.log("cookies set");
     // console.log(cookie);
