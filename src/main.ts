603f2175b8a6aaf2dc28fab24ece7f94dbbd742e
anna <annaliang56@gmail.com>
2025-09-11 23:57:49 -0400

    Clean up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
+import './scss/styles.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
